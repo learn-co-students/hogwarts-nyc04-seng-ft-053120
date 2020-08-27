bf86b894fa66fb685c299bb4b5a9cce19a791621
Jackson Chen <jacksonchen@Jacksons-Air.home>
2020-08-27 18:55:57 -0400

    Done!

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,10 +5,10 @@ class Options extends React.Component {
     render() {
         return (
             <div className="navWrapper">
-                <label for="greased">Greased: </label>
+                <label>Greased only? </label>
                 <input onChange={this.props.handleFilter} name="greased" type="checkbox" checked={this.props.filtered}></input>
                 <br/>
-                <label for="sort">Sort By: </label>
+                <label>Sort By: </label>
                 <select onChange={this.props.handleSort} name="sort">
                     <option value="name">Name</option>
                     <option value="weight">Weight</option>
